3b10763641917c431c9138cf2b532cb658bf83be
Sam <375522026@qq.com>
2022-03-29 15:54:49 +0800

    add singly linked list

---
+++ Data-Structure/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Data_Structure
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyListNode<T> Head { get; set; }
+    }
+
+    public class SinglyListNode<T>
+    {
+        public T Value { get; set; }
+        public SinglyListNode<T> Next { get; set; }
+
+        public SinglyListNode(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+
+}
